1a84e5a1786cffbbd246766838db90b0326a8e2f
Mercedes Bastardo <118407079+MercedesBastardo@users.noreply.github.com>
2023-01-31 23:28:16 -0300

    advances

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -21,7 +21,7 @@ const Home = () => {
       axios
       .get(`https://e-commerce-api.academlo.tech/api/v1/products/categories`)
       .then(resp => setCategorie(resp.data.data.categories))
-      .catch (error => console.log(error))
+      .catch(error => console.log(error))
       .finally(() => {
         setTimeout(() => {
           dispatch(setIsLoading(false))
@@ -36,13 +36,13 @@ const Home = () => {
     const submit = (data) => {
       dispatch(getFilterPrice(data))  
     }
-  
+
   return (
     <div className='content-home'>
       <h1>Home</h1>
 {/* Search */}
-      <Col className="navbar navbar-light border-0">
-          <Form className="d-flex" 
+      <Col className="search">
+          <Form 
           onSubmit={ (e) => searchProduct(e.target[0].value)}>
             <Form.Control
               onChange={(e) => searchProduct(e.target.value)}
@@ -50,20 +50,19 @@ const Home = () => {
               placeholder="What are you looking for?"
               className="me-2"
               aria-label="Search"
-              style={{heigth: '40px', width: '60vw'}}
             />       
           </Form>
       </Col>
 
-      <Row xs={2} md={3} lg={4}>
-
-      <Col className='filters d-block p-5' >
-        <div className='h-50'>
+      <div className='d-flex'>
+      <Row>
+      <Col className='d-block p-2' >
         {/* Dezpliegue de los precios */}
-      <Accordion defaultActiveKey="0" >
-        <Accordion.Item eventKey="0" className='border-0'>
-          <Accordion.Header className='w-auto shadow mb-1 bg-white text-dark border-bottom-dark rounded'  variant="success" id="dropdown-basic" style={{ width: '200px'}}>Price</Accordion.Header>
-          <Accordion.Body className='w-auto' show = 'onToggle' style={{ width: '200px'}}>
+        <div className='filtersPrice'>
+          <Accordion defaultActiveKey="0" >
+            <Accordion.Item eventKey="0" className='border-0'>
+            <Accordion.Header className='w-auto shadow mb-1 bg-white text-dark border-bottom-dark rounded'  variant="success" id="dropdown-basic" style={{ width: '200px'}}>Price</Accordion.Header>
+            <Accordion.Body className='w-auto' show = 'onToggle' style={{ width: '200px'}}>
             <Form onSubmit={ handleSubmit(submit) } >
                 <Form.Group className="mb-3" controlId="formBasicPriceOne">
                   <Form.Label>From</Form.Label>
@@ -80,9 +79,9 @@ const Home = () => {
           </Accordion.Body>
         </Accordion.Item>
       </Accordion>
-      </div> 
+        </div> 
 {/* Dezpliegue de las categorias */}
-      <div className='h-50'>
+      <div className='filtersCategorie'>
       <Accordion defaultActiveKey="0">
         <Accordion.Item eventKey="0" className='border-0'>
           <Accordion.Header className='w-auto shadow mb-1 bg-white text-dark border-bottom-dark rounded'  variant="success" id="dropdown-basic" style={{ width: '200px'}}>Category</Accordion.Header>
@@ -105,41 +104,42 @@ const Home = () => {
       </Accordion> 
       </div>
       </Col>
-{/* Listado completo de los productos */}
-
-  {
-    products.map((product, index) => (
-      <Col className='product d-flex' key={index} as={ Link } to = {`/products/${product.id}`}>
-        <Card className='Card' style={{height: '500px', width:'280px'}}>
-          <div className='homeImage'>
-            <Card.Img className='over'          
-            variant="top"
-            // Imagen 1
-            src={product.productImgs[0]} />
-            <Card.Img  
-            className='over1'          
-            variant="top"
-            // Imagen 2
-            src={product.productImgs[1]} />
-          </div>   
-          <Card.Body>
-            <div className='description'>
-              <Card.Text className='categorie'>{product.category.name}</Card.Text>
-              <Card.Title className='title'>{product.title}</Card.Title>
-              <Card.Text className='price'> <span>Price: </span> </Card.Text>
-              <Card.Text className='amount'>{product.price}</Card.Text>
-            </div> 
-            <Link to={'/purchase'} ><Button className='btn-shopping' variant="primary">
-                  <i className="fa-solid fa-cart-shopping"></i>
-            </Button>
-            </Link>
-          </Card.Body>
-        </Card>
-      </Col>
-    ))
-  } 
-  
       </Row>
+      <Row xs={1} md={2} lg={3} style={{width: '80%', paddingLeft: "50px" }}>
+      {/* Listado completo de los productos */}
+          {
+            products.map((product, index) => (
+              <Col className='product d-flex' key={index} as={ Link } to = {`/products/${product.id}`}>
+                <Card className='Card' style={{height: '500px', width:'280px'}}>
+                  <div className='homeImage'>
+                    <Card.Img className='over'          
+                    variant="top"
+                    // Imagen 1
+                    src={product.productImgs[0]} />
+                    <Card.Img  
+                    className='over1'          
+                    variant="top"
+                    // Imagen 2
+                    src={product.productImgs[1]} />
+                  </div>   
+                  <Card.Body>
+                    <div className='description'>
+                      <Card.Text className='categorie'>{product.category.name}</Card.Text>
+                      <Card.Title className='title'>{product.title}</Card.Title>
+                      <Card.Text className='price'> <span>Price: </span> </Card.Text>
+                      <Card.Text className='amount'>{product.price}</Card.Text>
+                    </div> 
+                    <Link to={'/purchase'} ><Button className='btn-shopping' variant="primary">
+                          <i className="fa-solid fa-cart-shopping"></i>
+                    </Button>
+                    </Link>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))
+          }  
+      </Row>
+      </div>
     </div>
   )
 }
